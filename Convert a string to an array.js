a973c434fdec92c0b232dfaa4063c2a3a8c7cfc3
Josiane Iradukunda <malaikajiradukunda@gmail.com>
2026-09-15 23:54:41 -0900

    Create Convert a string to an array.js

---
+++ Convert a string to an array.js
@@ -0,0 +1,14 @@
+//Write a function to split a string of space-separated words and convert it into an array of words. 
+//Words in the input will be separated by exactly one space. The input will not have leading or trailing spaces. 
+//A "word" is any contiguous sequence of characters that does not contain any space.
+
+//Examples (Input ==> Output):
+//"string" ==> ["string"]
+//"Robin Singh" ==> ["Robin", "Singh"]
+
+function stringToArray(string){
+
+  return string.split(" ");
+	// code code code
+
+}
